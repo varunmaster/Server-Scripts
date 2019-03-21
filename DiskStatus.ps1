67b1b4ca8280bf31240c892dd3d6ed077311970a
Varun Master <varunmaster95@gmail.com>
2019-03-20 23:27:03 -0400

    initial commit

---
+++ DiskStatus.ps1
@@ -0,0 +1,8 @@
+﻿$C = (gwmi win32_logicaldisk)[1] | select @{Name="Drive";Expression={"C:\"}}, @{Name="Total Size";Expression={$_.size/1GB}}, @{Name="Available Space";Expression={$_.FreeSpace/1GB}}, @{Name="Used Space";Expression={($_.Size/1GB)-($_.FreeSpace/1GB)}}
+$E = (gwmi win32_logicaldisk)[3] | select @{Name="Drive";Expression={"E:\"}}, @{Name="Total Size";Expression={$_.size/1GB}}, @{Name="Available Space";Expression={$_.FreeSpace/1GB}}, @{Name="Used Space";Expression={($_.Size/1GB)-($_.FreeSpace/1GB)}}
+
+$body = "<Table border='1'><tr><th><b>Drive Letter</b></th><th><b>Total Size(GB)</b></th><th><b>Available Space(GB)</b></th><th><b>Used Space(GB)</b></th>" + `
+        "<tr><td>" + $C.Drive + "</td>" + "<td>" + $C.'Total Size' + "</td>" + "<td>" + $C.'Available Space' + "</td>" + "<td>" + $C.'Used Space' + "</td>" + "</tr>" + `
+        "<tr><td>" + $E.Drive + "</td>" + "<td>" + $E.'Total Size' + "</td>" + "<td>" + $E.'Available Space' + "</td>" + "<td>" + $E.'Used Space' + "</td>" + "</tr></Table>"
+
+Send-MailMessage -SMTPServer localhost -To @("varunmaster95@gmail.com","nvelani2@gmail.com") -From Plex@ESXI-Plex.com -Subject "Plex Disk Usage - $(Get-Date -Format G)" -Body "$body" -BodyAsHtml
---
+++ Movie_Inventory.ps1
@@ -0,0 +1,18 @@
+﻿$movieList = @()
+$movieDir = "C:\Data\Movies"
+$date = Get-Date -Format G
+$movieFile = "C:\Data\movieList.txt"
+
+Clear-Content $movieFile
+
+foreach($item in (Get-ChildItem $movieDir)){
+    $movieList += "<tr><td>" + $($item.Name) + "</td><td>" + $($item.CreationTime) + "</td></tr>"
+}
+
+"Movie List as of: $date <br /> <Table border='1'><tr><th><b>Movie Name</b></th><th><b>First added to server</b></th></tr>" | Out-File -FilePath $movieFile -Append -NoClobber -Encoding utf8
+$movieList | Out-File -FilePath $movieFile -Append -NoClobber -Encoding utf8
+"</Table>" | Out-File -FilePath $movieFile -Append -NoClobber -Encoding utf8
+
+$body = Get-Content "C:\Data\movieList.txt" -Raw
+
+Send-MailMessage -SMTPServer localhost -To @("varunmaster95@gmail.com","nvelani2@gmail.com") -From Plex@ESXI-Plex.com -Subject "Plex Movie List - $date" -Body "$body" -BodyAsHtml
