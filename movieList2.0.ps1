270d75b8da682a9b3650d4b6550feb0bc04a9eff
Varun Master <varunmaster95@gmail.com>
2019-05-25 14:49:02 -0400

    adding api ps1 scripts

---
+++ movieList2.0.ps1
@@ -0,0 +1,37 @@
+﻿$movieDir = "C:\Data\Movies\*"
+$movieListTemp = @((Get-ChildItem $movieDir -File -Recurse -Exclude "*.txt","*.srt","*.jpg","*.ass","*.nfo","*.url","*.db","*.png").FullName)
+$movieList = @()
+$date = Get-Date -Format G
+$movieFile = "C:\Users\Administrator\app\routes\movieList.txt"
+
+if (!(Test-Path $movieFile))
+{
+   New-Item -path C:\Users\Administrator\app\routes -Name movieList.txt -type "file"
+}
+
+Clear-Content $movieFile
+
+for($i=0; $i -lt $movieListTemp.Length; $i++){
+    $movieList += "<tr><td>" + ((Split-Path $movieListTemp[$i] -Leaf) -Replace '\.',' ' `
+                                                                      -Replace 'x264','' `
+                                                                      -Replace 'BrRip.','' `
+                                                                      -Replace 'BDRip.','' `
+                                                                      -Replace 'BRRip.','' `
+                                                                      -Replace 'BluRay','' `
+                                                                      -Replace 'avi','' `
+                                                                      -Replace 'mp4','' `
+                                                                      -Replace 'mkv','' `
+                                                                      -Replace '\[YTS AM\]','' `
+                                                                      -Replace '\[YTS AG\]','' `
+                                                                      -Replace 'YIFY','' `
+                                                                      -Replace '1080p','' `
+                                                                      -Replace '720p','' `
+                                                                      -Replace 'DVDRip','' `
+                                                                      -Replace '-','' `
+                                                                      -Replace 'WEBRip','') + "</td><td>" + (gci $movieListTemp)[$i].CreationTime.ToString("MM/dd/yyyy") + "</td></tr>"
+}
+
+"Movie List as of: $date <br /> <Table border='1'><tr><th><b>Movie Name</b></th><th><b>First added</b></th></tr>" | Out-File -FilePath $movieFile -Append -NoClobber -Encoding utf8
+$movieList | Out-File -FilePath $movieFile -Append -NoClobber -Encoding utf8
+"<tr><td><b>Count</b></td> <td><b>$($movieListTemp.Count)</b></tr></td>" | Out-File -FilePath $movieFile -Append -NoClobber -Encoding utf8
+"</Table>" | Out-File -FilePath $movieFile -Append -NoClobber -Encoding utf8
---
+++ showList.ps1
@@ -0,0 +1,19 @@
+﻿$showListTemp = @((gci E:\Files\Shows).Name)
+$showList = @()
+$date = Get-Date -Format G
+$showFile = "C:\Users\Administrator\app\routes\showList.txt"
+
+if (!(Test-Path $showFile))
+{
+   New-Item -path C:\Users\Administrator\app\routes -Name showList.txt -type "file"
+}
+
+Clear-Content $showFile
+
+foreach ($show IN $showListTemp){
+    $showList += "<tr><td>" + $show + "</td></tr>"
+}
+
+"Show List as of: $date <br /> <Table border='1'><tr><th><b>TV Show</b></th></tr>" | Out-File -FilePath $showFile -Append -NoClobber -Encoding utf8
+$showList | Out-File -FilePath $showFile -Append -NoClobber -Encoding utf8
+"</Table>" | Out-File -FilePath $showFile -Append -NoClobber -Encoding utf8
